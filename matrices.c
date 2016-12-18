b2741767914620bc3e320fecedd9eb6643df937c
Jakub Smetana <jakubsmetana@gmail.com>
2016-12-18 16:53:09 +0100

    First functional version of matrices

--- matrices.c
+++ matrices.c
@@ -1,48 +1,58 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int first[4][4], second[4][4], third[4][4], count, arrnum, i, j, k, field;
-	scanf("%d", &count);
+
+
+	scanf ("%d", &count);
+	if (count < 2) {
+		printf ("Enter at least 2 matrices");
+		return -1;
+	}
+
 	field = 0;
-	arrnum = 0;
+	arrnum = 1;
+
 
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 4; j++)
-		scanf("%d", &first[i][j]);
+			scanf ("%d", &first[i][j]);
 	}
 
+	while (arrnum < count) {
 		for (i = 0; i < 4; i++) {
 			for (j = 0; j < 4; j++)
-			scanf("%d", &second[i][j]);
+				scanf ("%d", &second[i][j]);
 		}
 
 		for (i = 0; i < 4; i++) {
 			for (j = 0; j < 4; j++) {
-				for(k = 0; k < 4; k++) {
+				for (k = 0; k < 4; k++) {
 					field += first[i][k] * second[k][j];
 				}
 				third[i][j] = field;
-				printf("%d ", field);
 				field = 0;
 			}
 		}
 
-
-
-	
-
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 4; j++) {
-			printf("%d ", first[i][j]);
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 4; j++) {
+				first[i][j] = third[i][j];
+			}
 		}
-		printf("\n");
+
+		arrnum++;
 	}
 
+
+
+
+
 	for (i = 0; i < 4; i++) {
 		for (j = 0; j < 4; j++) {
-			printf("%d ", second[i][j]);
+			printf ("%d ", first[i][j]);
 		}
-		printf("\n");
-
+		printf ("\n");
 	}
 }
