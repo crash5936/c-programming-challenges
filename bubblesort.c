21556ee1344a32a1ade12eaf64faa0c9d92237f5
Jakub Smetana <jakubsmetana@gmail.com>
2016-12-23 11:40:00 +0100

    bubblesort.c beautified

--- bubblesort.c
+++ bubblesort.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(long* a, long* b) {
+void swap (long* a, long* b)
+{
 	long tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-int main() {
+int main()
+{
 	char swapped = 1;
 	int n;
 	scanf ("%d", &n);
-	long int* nums = malloc (n * sizeof(int));
+	long* nums = malloc (n * sizeof (int));
 	for (int i = 0; i < n; i++)
 		scanf ("%li", &nums[i]);
 
 
 	for (int i = 0; i < n; i++)
 		printf ("%li ", nums[i]);
-	printf("\n");
+	printf ("\n");
 
 	while (swapped != 0) {
 		swapped = 0;
 		for (int i = 0; i < n - 1; i++) {
 			if (nums[i] > nums[i + 1]) {
-				swap(&nums[i], &nums[i+1]);
+				swap (&nums[i], &nums[i + 1]);
 				swapped = 1;
 			}
 
@@ -35,7 +37,7 @@ int main() {
 
 	for (int i = 0; i < n; i++)
 		printf ("%li ", nums[i]);
-	printf("\n");
+	printf ("\n");
 
 	return 0;
 }
