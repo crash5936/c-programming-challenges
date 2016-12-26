fbf95b9834fd7d8b237e7ee81630a84249376375
Jakub Smetana <jakubsmetana@gmail.com>
2016-12-26 19:15:06 +0100

    Modified to print out computing time

--- bubblesort.c
+++ bubblesort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void swap (long* a, long* b)
 {
@@ -13,14 +14,21 @@ int main()
 	char swapped = 1;
 	int n;
 	scanf ("%d", &n);
+	
+	clock_t start, end;
+	double time;
+
 	long* nums = malloc (n * sizeof (long));
 	for (int i = 0; i < n; i++)
 		scanf ("%li", &nums[i]);
 
+/*
+*	for (int i = 0; i < n; i++)
+*		printf ("%li ", nums[i]);
+*	printf ("\n");
+*/
 
-	for (int i = 0; i < n; i++)
-		printf ("%li ", nums[i]);
-	printf ("\n");
+	start = clock();
 
 	while (swapped != 0) {
 		swapped = 0;
@@ -28,12 +36,18 @@ int main()
 			if (nums[i] > nums[i + 1]) {
 				swap (&nums[i], &nums[i + 1]);
 				swapped = 1;
+				if (i % 10000 == 0)
+					printf("%ld passes\n", i * 100000);
 			}
 
 		}
 
 	}
+	
+	end = clock();
+	time = ((double) (end - start)) / CLOCKS_PER_SEC;
 
+	printf("Time taken: %f s\n", time);
 
 	for (int i = 0; i < n; i++)
 		printf ("%li ", nums[i]);
